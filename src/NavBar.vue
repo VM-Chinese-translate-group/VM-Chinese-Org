7c7d0e99f87eaf4b71abe303a06b5bd3dc09ee5e
Litchiiiiiii <139540811+Litchiiiiii@users.noreply.github.com>
2025-09-03 08:18:05 +0000

    yes

---
+++ src/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar">
+    <ul class="navbar-list">
+      <li class="navbar-icon">
+        <img src="/vm_title_icon.ico" alt="Logo" class="navbar-logo" />
+        <span class="navbar-title">VM汉化组</span>
+      </li>
+      <li class="navbar-item"><a href="/modpack">整合包汉化</a></li>
+      <li class="navbar-item"><a href="/map">地图汉化</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="css">
+@import './NavBar.css';
+</style>
