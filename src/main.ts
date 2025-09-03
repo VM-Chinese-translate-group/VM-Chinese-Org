207585fe5c0268df5361261fed98cb825630635a
Litchiiiiiii <139540811+Litchiiiiii@users.noreply.github.com>
2025-09-03 09:39:40 +0000

    update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import { Icon } from '@iconify/vue'
+const app = createApp(App)
+app.component('Icon', Icon)
 
-createApp(App).mount('#app')
+app.mount('#app')
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
