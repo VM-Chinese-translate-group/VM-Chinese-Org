49a50b3420719838990239d7bdd1de579966e92c
Litchiiiiiii <139540811+Litchiiiiii@users.noreply.github.com>
2025-09-04 08:40:32 +0000

    适配样式

--- src/components/Main-Content/Main-Content.vue
+++ src/components/Main-Content/Main-Content.vue
@@ -6,8 +6,23 @@
         <img src="/vmicon.png" alt="" />
         <p>VM汉化组</p>
         <p>一个用爱发电专注于 汉化MC整合包与地图的非盈利组织</p>
-        <div class="button-div"><button>整合包汉化</button><button>地图汉化</button></div>
+        <div class="button-div">
+          <button>
+            整合包汉化
+            <Icon icon="fluent:arrow-right-48-filled" class="arrow-icon" />
+          </button>
+          <button>
+            地图汉化
+            <Icon icon="fluent:arrow-right-48-filled" class="arrow-icon" />
+          </button>
+        </div>
       </div>
+    </div>
+    <div class="other-content">
+      <div class="info-box"></div>
+      <div class="info-box"></div>
+      <div class="info-box"></div>
+      <div class="info-box"></div>
     </div>
   </main>
 </template>
