491c4ab80e6a135e4d90cc899189e58fb75ebbe2
Litchiiiiiii <139540811+Litchiiiiii@users.noreply.github.com>
2025-09-04 08:51:24 +0000

    info update

--- src/components/Main-Content/Main-Content.vue
+++ src/components/Main-Content/Main-Content.vue
@@ -19,10 +19,34 @@
       </div>
     </div>
     <div class="other-content">
-      <div class="info-box"></div>
-      <div class="info-box"></div>
-      <div class="info-box"></div>
-      <div class="info-box"></div>
+      <div class="info-box">
+        <h3>严格的质量把关</h3>
+        <p>
+          VM汉化组坚守严格的质量把关，将翻译质量 提升至新的高度。我们以精益求精的态度，追求
+          每一个细节的完美。从翻译准确性到文化本土化， 每一个环节都经过严密的审查与校对。
+        </p>
+      </div>
+      <div class="info-box">
+        <h3>热爱与团结</h3>
+        <p>
+          VM汉化组由一群具有相同爱好的小伙伴组成， 我们携手并肩，用爱发电。彼此相互支持，建
+          立并成为一个无坚不摧的团队。
+        </p>
+      </div>
+      <div class="info-box">
+        <h3>尊重版权️</h3>
+        <p>
+          我们深知作者的付出和保护知识产权的重要性。 绝大多数翻译都经过作者官方授权，不分发游戏
+          本体。仅提供汉化补丁下载，尽最大化保证作者 权益得到保障。
+        </p>
+      </div>
+      <div class="info-box">
+        <h3>技术驱动</h3>
+        <p>
+          我们深知技术是汉化环节中必不可少的部分， 为此我们制作了一套本地化工具、并且制作了
+          一个用于汉化更新检测的mod。
+        </p>
+      </div>
     </div>
   </main>
 </template>
