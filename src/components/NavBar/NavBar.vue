0877a877fbe77f1560ff68c3a438e61dc2a952a3
Wulian233 <1055917385@qq.com>
2026-03-23 20:38:57 +0800

    fix

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -32,16 +32,7 @@
 
         <div class="navbar-utils">
           <div class="search-box-trigger desktop-search" @click="openSearch">
-            <svg class="search-icon" viewBox="0 0 24 24" width="18" height="18">
-              <path
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m21 21l-4.343-4.343m0 0A8 8 0 1 0 5.343 5.343a8 8 0 0 0 11.314 11.314Z"
-              />
-            </svg>
+            <Icon icon="lucide:search" class="search-icon" />
             <span class="search-text">{{ $t('search.placeholder') }}</span>
             <span class="search-shortcut">Ctrl K</span>
           </div>
